37e10ecf0651d5265726e924feee09263dca9f5e
迎风 <adermoi99@live.com>
2017-07-01 16:50:06 +0800

    新加登录界面

---
+++ Deluxe.MainWeb/Areas/Back_Permission/Back_PermissionAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Deluxe.MainWeb.Areas.Back_Permission
+{
+    public class Back_PermissionAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Back_Permission";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Back_Permission_default",
+                "Back_Permission/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Deluxe.MainWeb/Areas/Back_Permission/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Deluxe.MainWeb.Areas.Back_Permission.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Back_Permission/Auth
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
